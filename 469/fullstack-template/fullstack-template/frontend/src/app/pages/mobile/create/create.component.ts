54b7e5fa2c94ba2193576a792ad341d5c869916f
Stelios Michelakakis <csd3524@csd.uoc.gr>
2019-11-20 23:16:20 +0200

    . env apo gitignore v2

--- 469/fullstack-template/fullstack-template/frontend/src/app/pages/mobile/create/create.component.ts
+++ 469/fullstack-template/fullstack-template/frontend/src/app/pages/mobile/create/create.component.ts
@@ -1,10 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+
 
 @Component({
   selector: 'ami-fullstack-create',
   templateUrl: './create.component.html',
+  template: `
+            <div class="ad-banner-example">
+              <head>Create Game</head>
+              <ng-template  ami-fullstack-create,
+              ></ng-template>
+            </div>
+            <h2>
+              Total Players
+            </h2>
+          `,
   styleUrls: ['./create.component.scss']
 })
+
+
 export class CreateComponent implements OnInit {
 
   constructor() { }
